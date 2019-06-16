62c9fc1d27f0a57773949c7bac355664a3680af4
刘小明 <liuxiaoming@liuxiaomingdeMacBook-Pro.local>
2019-06-16 22:05:25 +0800

    Initial commit

---
+++ src/main/java/com/liuxm/youngmemory/YoungmemoryApplication.java
@@ -0,0 +1,13 @@
+package com.liuxm.youngmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungmemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungmemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuxm/youngmemory/YoungmemoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liuxm.youngmemory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YoungmemoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
