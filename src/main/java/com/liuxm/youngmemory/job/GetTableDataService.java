67029111b2d768f99729a1403dd490bd7e7ce284
xmlrambo <384276224@qq.com>
2019-06-24 20:11:08 +0800

    定时任务，测试后台与数据库连通性

---
+++ src/main/java/com/liuxm/youngmemory/job/GetTableDataService.java
@@ -0,0 +1,29 @@
+package com.liuxm.youngmemory.job;
+
+import com.liuxm.youngmemory.mapper.GetTableDataMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Configuration
+@EnableScheduling
+public class GetTableDataService {
+    private static final Logger logger = LoggerFactory.getLogger(GetTableDataService.class);
+
+    @Autowired
+    private GetTableDataMapper getTableDataMapper;
+
+    //@Scheduled(cron = "*/1 * * * * ?")
+    public void execute() throws Exception{
+        List resultList = getTableDataMapper.getTableData();
+        System.out.println("123");
+        System.out.println(resultList);
+    }
+}
---
+++ src/main/java/com/liuxm/youngmemory/mapper/GetTableDataMapper.java
@@ -0,0 +1,7 @@
+package com.liuxm.youngmemory.mapper;
+
+import java.util.List;
+
+public interface GetTableDataMapper {
+    List getTableData();
+}
